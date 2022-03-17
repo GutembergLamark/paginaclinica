ce5e8836e70df5271dcc3e22eb71815c5bb4b7cc
Gutemberg Lamark <89531845+GutembergLamark@users.noreply.github.com>
2022-03-16 23:57:36 -0300

    Adicionando pagina da clinica

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css"
+
+const Rodape = () => {
+    return(
+        <footer className="rodape">
+            <div className="limitar-container">
+                © Copyright 2021   |   Gutemberg Lamark
+            </div>
+        </footer>
+    );
+}
+
+export default Rodape;
