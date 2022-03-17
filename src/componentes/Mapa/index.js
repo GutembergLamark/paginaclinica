39e4bafae719a8ad98d43eb6bd5e66dce8263974
Gutemberg Lamark <89531845+GutembergLamark@users.noreply.github.com>
2022-03-17 00:02:07 -0300

    Update index.js

--- src/componentes/Mapa/index.js
+++ src/componentes/Mapa/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContatoMapa = () => {
     return(
         <div className="contato-rota">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.1168143961863!2d-43.36308168555829!3d-22.98273154636219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bda2ed54ec2e1%3A0x4431d262cad1d163!2sAv.%20Ayrton%20Senna%2C%203000%20-%20Barra%20da%20Tijuca%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2022775-904!5e0!3m2!1spt-BR!2sbr!4v1647391666171!5m2!1spt-BR!2sbr" width="100%" height="350"></iframe> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.1168143961863!2d-43.36308168555829!3d-22.98273154636219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bda2ed54ec2e1%3A0x4431d262cad1d163!2sAv.%20Ayrton%20Senna%2C%203000%20-%20Barra%20da%20Tijuca%2C%20Rio%20de%20Janeiro%20-%20RJ%2C%2022775-904!5e0!3m2!1spt-BR!2sbr!4v1647391666171!5m2!1spt-BR!2sbr" width="100%" height="350" iframe-has-title="mapa-local"></iframe> 
         </div>
     )
 }
